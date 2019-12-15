c9faeb7f9aeeeed9161c9f12b1eda03ecbb3c0e4
ov-vlad10 <56393569+ov-vlad10@users.noreply.github.com>
2019-12-15 19:41:05 +0300

    Add files via upload

---
+++ lab_6/lab_6/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Car c1 = new Car("kia", "ceed", "sw", 5220000, 2009);
+        Car c2 = new Car("BMW", "x5", "Cross", 4265000, 2016);
+
+        c1.println();
+        c2.println();
+    }
+}
+
